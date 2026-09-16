d11680b62356753b0b8a41ac24cc7709c5988ecd
Kuber <138642048+kuber0007@users.noreply.github.com>
2026-09-16 15:00:56 +0530

    test2

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
-  // import.meta.env.VITE_API_URL ||
-  "http://localhost:8000/api";
+  import.meta.env.VITE_API_URL
+  // "http://localhost:8000/api";
 
 const apiRequest = async (
   endpoint,
